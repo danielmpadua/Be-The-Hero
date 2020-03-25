d6276acc1a45ba9c5defc6fb3bd5a92d1ba6b25b
Daniel Pádua <daniel.mpadua@outlook.com>
2020-03-25 19:46:17 -0300

    Frontend

---
+++ Frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Routes from './routes';
+
+import './global.css';
+import { Route } from 'react-router-dom';
+
+// JSX (JavaScript XML)
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
